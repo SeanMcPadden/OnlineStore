05f37ad8c53749b889fa44279736ab574e17cb39
sean mc padden <seanmcpadden@hotmail.com>
2022-09-23 23:26:36 +0100

    Deleted unused models

--- christmasinbelfast/Models/CachingForPurchases.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace christmasinbelfast.Models
-{
-    public class CachingForPurchases
-    {
-        public List<Purchase> CachingPurchases { get; set; }
-    }
-}
